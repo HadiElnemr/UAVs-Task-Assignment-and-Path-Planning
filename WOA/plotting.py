a253044c98f1a2a9aa2a103bc0210fc5bd3a7b3c
HadiElnemr <hadi.elnemr@gmail.com>
2022-12-16 20:36:27 +0200

    add plot paths function

--- WOA/plotting.py
+++ WOA/plotting.py
@@ -1,9 +1,30 @@
 from matplotlib import pyplot as plt
-  
+from classes import *
+from typing import List
+
 def plot_fitnesses(n_iter, best_fitnesses):
     fig1, ax1 = plt.subplots()
     ax1.set_xlabel('Iteration no.')
     ax1.set_ylabel('fitness value')
 
     ax1.plot([i for i in range(1,n_iter+1)], best_fitnesses)
+    plt.show()
+
+def plot_paths(uavs:List[UAV], tasks:List[Task], map_dim):
+    for task in tasks:
+        plt.plot(task.position.x, task.position.y, marker="o", markersize=14, markerfacecolor="green", markeredgecolor='green')
+    
+    for i,uav in enumerate(uavs):
+        points_x = [uav.position.x]
+        points_y = [uav.position.y]
+        plt.plot(uav.position.x, uav.position.y, marker="o", markersize=12, markerfacecolor="red")
+        
+        for point in uav.path:
+            points_x += [point.x]
+            points_y += [point.y]
+        plt.plot(points_x, points_y, '-o', markersize=5)
+
+    plt.xlim([0, map_dim])
+    plt.ylim([0, map_dim])
+    plt.grid()
     plt.show()
